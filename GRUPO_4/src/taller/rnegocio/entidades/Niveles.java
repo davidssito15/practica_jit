393f16e3946df3850c37f6c61803263102ecdfbc
Segovia <Segovia@172.30.59.213>
2018-01-11 18:27:15 -0500

    inicio de proyecto

---
+++ GRUPO_4/src/taller/rnegocio/entidades/Niveles.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller.rnegocio.entidades;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Segovia
+ */
+public class Niveles {
+    private String nombre;
+    private Date crear;
+    private Date actulizar;
+
+    public Niveles() {
+    }
+
+    public Niveles(String nombre, Date crear, Date actulizar) {
+        this.nombre = nombre;
+        this.crear = crear;
+        this.actulizar = actulizar;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getCrear() {
+        return crear;
+    }
+
+    public void setCrear(Date crear) {
+        this.crear = crear;
+    }
+
+    public Date getActulizar() {
+        return actulizar;
+    }
+
+    public void setActulizar(Date actulizar) {
+        this.actulizar = actulizar;
+    }
+    
+    
+}
