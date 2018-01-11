ffc8e7271ea602c21985546630fba899d5b96331
Usuario <Usuario@172.30.3.173>
2018-01-11 17:50:04 -0500

    grupo 4:Campos, Troya,Segovia, Amancha

--- GRUPO_4/src/taller/rnegocio/entidades/Etiquetas.java
+++ GRUPO_4/src/taller/rnegocio/entidades/Etiquetas.java
@@ -1,5 +1,52 @@
 package taller.rnegocio.entidades;
 
 public class Etiquetas {
+  private int idetiqueta;
+  private String nombre; 
+  private String creado ;
+  private String actualizado;
+
+    public Etiquetas() {
+    }
+
+    public Etiquetas(int idetiqueta, String nombre, String creado, String actualizado) {
+        this.idetiqueta = idetiqueta;
+        this.nombre = nombre;
+        this.creado = creado;
+        this.actualizado = actualizado;
+    }
+
+    public int getIdetiqueta() {
+        return idetiqueta;
+    }
+
+    public void setIdetiqueta(int idetiqueta) {
+        this.idetiqueta = idetiqueta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCreado() {
+        return creado;
+    }
+
+    public void setCreado(String creado) {
+        this.creado = creado;
+    }
+
+    public String getActualizado() {
+        return actualizado;
+    }
+
+    public void setActualizado(String actualizado) {
+        this.actualizado = actualizado;
+    }
+  
     
 }
