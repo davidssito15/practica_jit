925a7def314a2946cebc419128378436533c74b4
Davidssito Campos <Davidssito Campos@192.168.1.116>
2018-01-15 01:51:17 -0500

    David Campos IRoles

--- GRUPO_4/src/grupo_4/dao/IRoles.java
+++ GRUPO_4/src/grupo_4/dao/IRoles.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo_4.dao;
-
-/**
- *
- * @author Davidssito Campos
- */
     import grupo_4.entidades.*;
     import java.util.*;
 public interface IRoles {
-    public int insertar (Roles roles) throws Exception;
-    public int modificar (Roles roles) throws Exception; 
-    public int eliminar (Roles roles) throws Exception;
-    public Roles obtener (String codigo) throws Exception;
+    public int insertar (Roles rol) throws Exception;
+    public int modificar (Roles rol) throws Exception; 
+    public int eliminar (Roles rol) throws Exception;
+    public Roles obtener (long id) throws Exception;
     public ArrayList<Roles> obtener() throws Exception;
     
 }
