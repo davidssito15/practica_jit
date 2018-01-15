5e6303be7b0377a424084dc21ef904bf51c8be32
Davidssito Campos <Davidssito Campos@192.168.1.116>
2018-01-15 01:58:48 -0500

    David Campos test roles

--- GRUPO_4/test/grupo_4/RolesTest.java
+++ GRUPO_4/test/grupo_4/RolesTest.java
@@ -1,20 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo_4;
-
+import grupo_4.accesodatos.*;
+import grupo_4.dao.*;
+import grupo_4.entidades.*;
+import grupo_4.impl.*;
+import org.junit.Test;
+import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Davidssito Campos
- */
 public class RolesTest {
     
     public RolesTest() {
     }
-    
+    @Test
+    public void testGeneral() {
+        IRoles rolDao=new ImplRoles();
+       //TEST INSERTAR
+        int filas=0;
+        Roles nuevoRol=new Roles(999, "GILDA", new Date(), new Date());
+        try {
+            filas=rolDao.insertar(nuevoRol);
+            System.out.println("filas Insertadas:"+filas+"\n");
+        } catch (Exception e) {
+        }
+        assertEquals(filas>0, true);
+//        //TEST OBTENER POR CODIGO
+        Roles role=new Roles();
+        try {
+            role=rolDao.obtener(2313);
+            System.out.println(role.getId()+"    "+role.getNombre()+"    "+role.getCreado()+"    "+role.getActualizado()+"\n");
+        } catch (Exception e) {
+        }
+//        //TEST LISTADO
+        ArrayList<Roles> roles=new ArrayList<>();
+        try {
+            roles=rolDao.obtener();
+            for(Roles rol:roles){
+                System.out.println(rol.getId()+" "+rol.getNombre()+" "+rol.getCreado()+" "+rol.getActualizado());
+            }
+        } catch (Exception e) {
+        }
+        assertTrue(roles.size()>0);
+    }
 }
