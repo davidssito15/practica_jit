5267f4bd661c71ef93971eae93c6319dff4a7829
Davidssito Campos <Davidssito Campos@192.168.1.116>
2018-01-15 02:35:40 -0500

    Sairy Segovia interface IEtiquetas

--- GRUPO_4/src/grupo_4/dao/IEtiquetas.java
+++ GRUPO_4/src/grupo_4/dao/IEtiquetas.java
@@ -1,20 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo_4.dao;
-
-/**
- *
- * @author Davidssito Campos
- */
 import grupo_4.entidades.*;
 import java.util.*;
 public interface IEtiquetas {
-    public int insertar (Etiquetas etiquetas) throws Exception;
-    public int modificar (Etiquetas etiquetas) throws Exception; 
-    public int eliminar (Etiquetas etiquetas) throws Exception;
-    public Etiquetas obtener (String codigo) throws Exception;
-    public ArrayList<Etiquetas> obtener() throws Exception;   
+     public int insertar (Etiquetas etiqueta) throws Exception;
+    public int modificar (Etiquetas etiqueta) throws Exception; 
+    public int eliminar (Etiquetas etiqueta) throws Exception;
+    public Etiquetas obtener (long id) throws Exception;
+    public ArrayList<Etiquetas> obtener() throws Exception;
 }
