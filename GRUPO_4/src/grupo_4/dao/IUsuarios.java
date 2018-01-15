fdfdc8a70f2741fb21fa084c5bcd656e1a54360d
Davidssito Campos <Davidssito Campos@192.168.1.116>
2018-01-15 02:18:25 -0500

    Marco Amancha Interface IUsuarios

--- GRUPO_4/src/grupo_4/dao/IUsuarios.java
+++ GRUPO_4/src/grupo_4/dao/IUsuarios.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package grupo_4.dao;
 
-/**
- *
- * @author Davidssito Campos
- */
     import grupo_4.entidades.*;
     import java.util.*;
 public interface IUsuarios {
-     public int insertar (Usuarios usuarios) throws Exception;
-    public int modificar (Usuarios usuarios) throws Exception; 
-    public int eliminar (Usuarios usuarios) throws Exception;
-    public Usuarios obtener (String codigo) throws Exception;
+    public int insertar (Usuarios usuario) throws Exception;
+     public int modificar (Usuarios usuario) throws Exception; 
+    public int eliminar (Usuarios usuario) throws Exception;
+    public Usuarios obtener (long id) throws Exception;
     public ArrayList<Usuarios> obtener() throws Exception;
     
 }
