50f27a70da18974fd7ab51e89f5f714a323168d4
Davidssito Campos <Davidssito Campos@192.168.1.116>
2018-01-15 02:42:21 -0500

    Sairy Segovia Test Etiquetas

--- GRUPO_4/test/grupo_4/EtiquetasTest.java
+++ GRUPO_4/test/grupo_4/EtiquetasTest.java
@@ -1,21 +1,47 @@
 package grupo_4;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+import grupo_4.accesodatos.*;
+import grupo_4.dao.*;
+import grupo_4.entidades.*;
+import grupo_4.impl.*;
+import org.junit.Test;
+import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-/**
- *
- * @author Davidssito Campos
- */
 public class EtiquetasTest {
+    
     
     public EtiquetasTest() {
     }
-    
+// @Test
+//
+//    public void testGeneral() {
+//          IEtiquetas etiquetaDao=new ImplEtiquetas();
+////        //TEST INSERTAR
+//        int filas=0;
+//        Etiquetas nuevoEtiqueta=new Etiquetas(1719, "sebas", new Date(), new Date());
+//        try {
+//            filas=etiquetaDao.insertar(nuevoEtiqueta);
+//            System.out.println("filas Insertadas:"+filas+"\n");
+//        } catch (Exception e) {
+//        }
+//        assertEquals(filas>0, true);
+////        //TEST OBTENER POR CODIGO
+//        Etiquetas etiquee=new Etiquetas();
+//        try {
+//            etiquee=etiquetaDao.obtener(1719);
+//            System.out.println(etiquee.getId()+"    "+etiquee.getNombre()+"    "+etiquee.getCreado()+"    "+etiquee.getActualizado()+"\n");
+//        } catch (Exception e) {
+//        }
+////        //TEST LISTADO
+//        ArrayList<Etiquetas> etiquetas=new ArrayList<>();
+//        try {
+//            etiquetas=etiquetaDao.obtener();
+//            for(Etiquetas etiqueta:etiquetas){
+//                System.out.println(etiqueta.getId()+" "+etiqueta.getNombre()+" "+etiqueta.getCreado()+" "+etiqueta.getActualizado());
+//            }
+//        } catch (Exception e) {
+//        }
+//        assertTrue(etiquetas.size()>0);
+//    }
 }
