5d2580a8a5e051a46a0752bc9b582bc9f877e7a5
Davidssito Campos <Davidssito Campos@192.168.1.116>
2018-01-15 02:12:22 -0500

    Gilda Troya test Niveles

--- GRUPO_4/test/grupo_4/NivelesTest.java
+++ GRUPO_4/test/grupo_4/NivelesTest.java
@@ -1,20 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package grupo_4;
 
+package grupo_4;
+import grupo_4.accesodatos.*;
+import grupo_4.dao.*;
+import grupo_4.entidades.*;
+import grupo_4.impl.*;
+import org.junit.Test;
+import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Davidssito Campos
- */
 public class NivelesTest {
     
     public NivelesTest() {
     }
-    
+     @Test
+    public void testGeneral(){
+        INiveles nivelDao=new ImplNiveles();
+//        //TEST INSERTAR
+        int filas=0;
+        Niveles nuevoNivel=new Niveles(222, "ANASTACIA", new Date(), new Date());
+        try {
+            filas=nivelDao.insertar(nuevoNivel);
+            System.out.println("filas Insertadas:"+filas+"\n");
+        } catch (Exception e) {
+        }
+        assertEquals(filas>0, true);
+//        //TEST OBTENER POR CODIGO
+        
+        Niveles nive=new Niveles();
+        try {
+            nive=nivelDao.obtener(2313);
+            System.out.println(nive.getId()+"    "+nive.getNombre()+"    "+nive.getCreado()+"    "+nive.getActualizado()+"\n");
+        } catch (Exception e) {
+        }
+//        //TEST LISTADO
+        ArrayList<Niveles> niveles=new ArrayList<>();
+        try {
+            niveles=nivelDao.obtener();
+            for(Niveles niv:niveles){
+                System.out.println(niv.getId()+" "+niv.getNombre()+" "+niv.getCreado()+" "+niv.getActualizado());
+            }
+        } catch (Exception e) {
+        }
+        assertTrue(niveles.size()>0);
+    }
 }
